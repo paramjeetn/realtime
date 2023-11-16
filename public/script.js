6b18a2e884b3bb1c5cc21592d19069c54c849ecb
Paramjeet <paramjeetnpradhan@gmail.com>
2023-11-16 06:01:19 +0530

    chnaged

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ const { username, room } = Qs.parse(location.search, {
    ignoreQueryPrefix: true,
 });
 
-const socket = io();
+const socket = io(https://relatime.onrender.com/);
 
 // Join chatroom
 socket.emit('joinRoom', { username, room });
